6595885cdb271741340dba2311feda43a4d9bc37
MUHAMMAD OSAMA <developerscripters@gmail.com>
2024-11-08 22:38:53 -0800

    navbar size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         scrollPosition > 50 ? 'bg-white' : 'bg-transparent'
       }`}
     >
-      <div className="py-8 flex items-center justify-between px-4 md:px-10 lg:px-10 xl:px-20 2xl:px-40">
+      <div className="py-4 flex items-center justify-between px-4 md:px-10 lg:px-10 xl:px-20 2xl:px-40">
 
         <div className="flex-shrink-0">
           <Link href='/'>
